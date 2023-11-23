c60191097f1db0aa0ae3690b99914bfec0187a99
Raymond Oluoch <rasholuoch@gmail.com>
2023-11-23 16:10:33 +0300

    game aliens

--- ship.py
+++ ship.py
@@ -23,6 +23,11 @@ class Ship:
     self.moving_right = False
     self.moving_left = False
 
+  def center_ship(self):
+    """Center the ship on the screen"""
+    self.rect.midbottom = self.screen_rect.midbottom
+    self.x = float(self.rect.x)
+
   def update(self):
     """Update the ships's position based on the movement flags"""
     # Update the ships's x value, not the rect
